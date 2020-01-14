e1fde5e00c66ca5ad586117851c99ca9e3561035
onelei <ahleiwolong@163.com>
2020-01-14 20:05:03 +0800

    *更新到unity2018.2.12f1 UGUI

--- unity/simpleSdk/Assets/Scripts/SDKManager.cs
+++ unity/simpleSdk/Assets/Scripts/SDKManager.cs
@@ -1,57 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class SDKManager : MonoBehaviour {
-	[SerializeField] private UILabel m_testAdd,m_testMult,m_testSend;
-	// Use this for initialization
-	void Start () {
-		testAdd ();
-		testMult ();
-		testSend ();
-	}
+    [SerializeField] private Text m_testAdd, m_testMult, m_testSend;
+    // Use this for initialization
+    void Start() {
+        testAdd();
+        testMult();
+        testSend();
+    }
 
-	void testAdd(){
-		var result = 0;
-		#if UNITY_IPHONE
+    void testAdd() {
+        var result = 0;
+#if UNITY_IPHONE
 		result = _Add(1,2);
-		#elif UNITY_ANDROID
-		AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");  
-		AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");  
-		result = jo.CallStatic<int>("add",1,2);  
-		#endif
-		m_testAdd.text = result.ToString();  
+#elif UNITY_ANDROID
+        AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");
+        result = jo.CallStatic<int>("add", 1, 2);
+#endif
+        m_testAdd.text = result.ToString();
 
-	}
+    }
 
-	void testMult(){
-		var result = 0;
-		#if UNITY_IPHONE
+    void testMult() {
+        var result = 0;
+#if UNITY_IPHONE
 		result = _Multiply(3,4);
-		#elif UNITY_ANDROID
-		AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");  
-		AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");  
-		result = jo.Call<int>("Multiply",3,4);  
-		#endif
-		m_testMult.text = result.ToString();  
-	}
+#elif UNITY_ANDROID
+        AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");
+        result = jo.Call<int>("Multiply", 3, 4);
+#endif
+        m_testMult.text = result.ToString();
+    }
 
-	void testSend(){
-		#if UNITY_IPHONE
+    void testSend() {
+#if UNITY_IPHONE
 		_sendToUnity();
-		#elif UNITY_ANDROID
-		AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");  
-		AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");  
-		jo.CallStatic("sendToUnity");  
-		#endif
-	
-	}
+#elif UNITY_ANDROID
+        AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.CallStatic("SendToUnity");
+#endif
 
-	void androidMessgae(string msg){
-		m_testSend.text = msg;
-	}
+    }
 
-	void iosCallBack(string msg){
+    public void Evaluate()
+    {
+        Debug.Log("Unity------------- Evaluate");
+        AndroidJavaClass ja = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = ja.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("Evaluate");
+        Debug.Log("Unity------------- Evaluate   end--------------");
+    }
+
+
+    public void AndroidMessage(string msg)
+    {
+        m_testSend.text = msg;
+    }
+
+    void iosCallBack(string msg){
 		m_testSend.text = msg;
 	}
 
